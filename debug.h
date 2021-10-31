d9fbe7ddca68164d9847f9e1ec6abaadede307f2
Heitor Adão Jr <heitor.adao@gmail.com>
2021-10-31 02:07:14 -0300

    Modernização do código
    - substituição dos hard tabs por espaços
    - adição de chaves para todos os `if`'s e `for`'s

--- debug.h
+++ debug.h
@@ -12,34 +12,35 @@ using std::endl;
 
 void debug_PreencheTabelaPontos(SPonto *tabela)
 {
-	tabela[0].x = 0;
-	tabela[0].y = 0;
-	tabela[1].x = 1;
-	tabela[1].y = 1;
-	tabela[2].x = 2;
-	tabela[2].y = 1;
-	tabela[3].x = 3;
-	tabela[3].y = 4;
-	tabela[4].x = 4;
-	tabela[4].y = 4;
+    tabela[0].x = 0;
+    tabela[0].y = 0;
+    tabela[1].x = 1;
+    tabela[1].y = 1;
+    tabela[2].x = 2;
+    tabela[2].y = 1;
+    tabela[3].x = 3;
+    tabela[3].y = 4;
+    tabela[4].x = 4;
+    tabela[4].y = 4;
 }
 
 void MostraMatrizCoeficientes(double **coeficientes, int grauPolinomio)
 {
-	for (int linha = 0; linha <= grauPolinomio; linha++)
-	{
-		for (int coluna = 0; coluna <= grauPolinomio; coluna++)
-			cout << coeficientes[linha][coluna] << ' ';
-		cout << endl;
-	}
-	cout << endl;
+    for (int linha = 0; linha <= grauPolinomio; linha++) {
+        for (int coluna = 0; coluna <= grauPolinomio; coluna++) {
+            cout << coeficientes[linha][coluna] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
 
 void MostraTermosIndependentes(double *independentes, int grauPolinomio)
 {
-	for (int linha = 0; linha <= grauPolinomio; linha++)
-		cout << independentes[linha] << endl;
-	cout << endl;
+    for (int linha = 0; linha <= grauPolinomio; linha++) {
+        cout << independentes[linha] << endl;
+    }
+    cout << endl;
 }
 
 #endif // H8OR_DEBUG
--- main.cpp
+++ main.cpp
@@ -7,20 +7,21 @@ using namespace std;
 
 int main()
 {
-	setprecision(16);
-	cout << "Grau do polinomio de ajuste: ";
-	int grau;
-	cin >> grau;
-	int numeroPontos;
+    setprecision(16);
+    cout << "Grau do polinomio de ajuste: ";
+    int grau;
+    cin >> grau;
+    int numeroPontos;
 
-	SPonto *tabela = PreencheTabela(&numeroPontos);
-	double **coeficientes = PreencheMatrizCoeficientes(tabela, 5, grau); // Construir a matriz dos coeficientes
-	double *independentes = PreencheVetorTermosIndependentes(tabela, 5, grau); // Montar o vetor dos termos independentes
+    SPonto *tabela = PreencheTabela(&numeroPontos);
+    double **coeficientes = PreencheMatrizCoeficientes(tabela, 5, grau); // Construir a matriz dos coeficientes
+    double *independentes = PreencheVetorTermosIndependentes(tabela, 5, grau); // Montar o vetor dos termos independentes
 
-	double *incognitas = new double[grau + 1];
-	GaussSeidel(coeficientes, grau + 1, incognitas, independentes, 5000000, 0.000000000001);
+    double *incognitas = new double[grau + 1];
+    GaussSeidel(coeficientes, grau + 1, incognitas, independentes, 5000000, 0.000000000001);
 
-	for (int i = 0; i <= grau; i++)
-		cout << "i = " << incognitas[i] << endl;
-	return 0;
+    for (int i = 0; i <= grau; i++) {
+        cout << "i = " << incognitas[i] << endl;
+    }
+    return 0;
 }
