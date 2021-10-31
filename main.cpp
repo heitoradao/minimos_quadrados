352d74f6ea1474c4e681ff38b66267646a405fe0
Heitor Adão Jr <heitor.adao@gmail.com>
2021-10-31 00:19:55 -0300

    Adiciona classe GaussSeidel
    Inclusão no cbp e adaptação do main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #include "debug.h"
 #include "mmq.h"
+#include "GaussSeidel.h"
 
 int main()
 {
@@ -16,7 +17,10 @@ int main()
 	double **coeficientes = PreencheMatrizCoeficientes(tabela, 5, grau); // Construir a matriz dos coeficientes
 	double *independentes = PreencheVetorTermosIndependentes(tabela, 5, grau); // Montar o vetor dos termos independentes
 
-	MostraMatrizCoeficientes(coeficientes, grau);
-	MostraTermosIndependentes(independentes, grau);
+	double *incognitas = new double[grau + 1];
+	GaussSeidel(coeficientes, grau + 1, incognitas, independentes, 5000000, 0.000000000001);
+
+	for (int i = 0; i <= grau; i++)
+		cout << "i = " << incognitas[i] << endl;
 	return 0;
 }
